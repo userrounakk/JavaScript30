bb7ecec42decc8d4dcca9c1c57bc13112a9a33f8
Rounak Agrawal <rounak1430@gmail.com>
2022-03-10 16:45:30 +0545

    DayEight

--- DayEight/script.js
+++ DayEight/script.js
@@ -4,7 +4,9 @@ var canvas = document.querySelector('#draw-board'),
     width = document.querySelector('#width'),
     eraser = document.querySelector('#eraser'),
     pen = document.querySelector('#pen'),
-    widthText = document.querySelector('#width-text');
+    downloadBtn = document.querySelector('#download'),
+    clear = document.querySelector('#clear')
+widthText = document.querySelector('#width-text');
 
 
 canvas.width = window.innerWidth * 0.9;
@@ -17,6 +19,8 @@ context.strokeStyle = '#fa6167';
 context.lineJoin = 'round';
 context.lineCap = 'round';
 context.lineWidth = 1;
+context.fillStyle = "#202124";
+context.fillRect(0, 0, canvas.width, canvas.height);
 
 
 let isDrawing = false;
@@ -33,13 +37,10 @@ color.addEventListener('change', () => {
     selected == 'eraser' ? context.strokeStyle = `#202124` : context.strokeStyle = `${color.value}`;
 });
 width.addEventListener('change', () => context.lineWidth = `${width.value}`);
-// eraser.addEventListener('change', () => {
-//     console.log(eraser.value);
-//     eraser.value == 'on' ? context.strokeStyle = `#202124` : context.strokeStyle = context.strokeStyle = `${color.value}`;
-// });
 eraser.addEventListener('click', selectedEraser);
 pen.addEventListener('click', selectedPen);
-
+downloadBtn.addEventListener('click', download);
+clear.addEventListener('click', clearCanvas);
 
 
 function draw(e) {
@@ -67,7 +68,7 @@ function selectedEraser(e) {
     color.setAttribute('disabled', '');
 }
 function selectedPen(e) {
-    e.preventDefault()
+    e.preventDefault();
     if (selected == 'pen') return
     this.classList.add('active');
     eraser.classList.remove('active');
@@ -75,4 +76,26 @@ function selectedPen(e) {
     context.strokeStyle = `${color.value}`;
     color.enabled = true;
     color.removeAttribute('disabled');
+}
+
+function download(e) {
+    e.preventDefault();
+    var a = document.createElement('a'), f;
+    a.href = canvas.toDataURL("image/png;base64");
+    a.download = "YourCanvasDrawing.png";
+    if (document.createEvent) {
+        f = document.createEvent("MouseEvents");
+        f.initMouseEvent("click", true, true, window,
+            0, 0, 0, 0, 0, false, false, false,
+            false, 0, null);
+
+        a.dispatchEvent(f);
+    } else if (a.fireEvent) {
+        a.fireEvent("onclick");
+    }
+}
+
+function clearCanvas(e) {
+    e.preventDefault();
+    context.clearRect(0, 0, canvas.width, canvas.height);
 }
